e57b3ce8cabdcbfc1964e0d9195691411eca6cb1
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-21 14:12:48 +0200

    Projectiles die

--- scenes/projectile/projectile.gd
+++ scenes/projectile/projectile.gd
@@ -6,4 +6,12 @@ var target_position: Vector2 = Vector2.ZERO
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	const MARGIN_DISTANCE := 5
 	if position.distance_to(target_position) <= MARGIN_DISTANCE:
-		state.linear_velocity = Vector2.ZERO
+		on_target_reached(state)
+
+func on_target_reached(physics_state: PhysicsDirectBodyState2D) -> void:
+	physics_state.linear_velocity = Vector2.ZERO
+	
+	create_tween().tween_interval(2).finished.connect(
+		func(): 
+		queue_free()
+		)
