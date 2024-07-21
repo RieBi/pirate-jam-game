d1ef9956986a0c588266aecdfed3c55180885717
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-21 14:23:57 +0200

    Add functions to player health component

--- scenes/components/component_player_health.gd
+++ scenes/components/component_player_health.gd
@@ -2,7 +2,18 @@ extends Node2D
 @export var max_health: int = 10
 var health = max_health
 
+signal health_gone
+
 @onready var label: Label = $Label
 
 func _process(_delta: float) -> void:
 	label.text = "Health: %s" % health
+
+func damage(dmg: int):
+	health -= dmg
+	if health <= 0:
+		health_gone.emit()
+
+func heal(amount: int):
+	health += amount
+	health = clampi(health, 0, max_health)
