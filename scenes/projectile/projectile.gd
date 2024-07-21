410289351923bbe1b9733d7595a14e21c89221ca
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-21 14:48:25 +0200

    Projectiles explode and kill

--- scenes/projectile/projectile.gd
+++ scenes/projectile/projectile.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var speed: int = 200
 var target_position: Vector2 = Vector2.ZERO
+var is_target_reached = false
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	const MARGIN_DISTANCE := 5
@@ -9,9 +10,23 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 		on_target_reached(state)
 
 func on_target_reached(physics_state: PhysicsDirectBodyState2D) -> void:
+	if is_target_reached:
+		return
+	is_target_reached = true
 	physics_state.linear_velocity = Vector2.ZERO
 	
-	create_tween().tween_interval(2).finished.connect(
-		func(): 
-		queue_free()
-		)
+	explode()
+	await create_tween().tween_interval(2).finished
+	queue_free()
+
+func explode() -> void:
+	$Sprite2D.queue_free()
+	var particles = $Explosion/CPUParticles2D as CPUParticles2D
+	particles.emitting = true
+	
+	var area = $Explosion/Area2D as Area2D
+	
+	for body in area.get_overlapping_bodies():
+		if body.is_in_group("enemy"):
+			body.queue_free()
+			pass # Damage enemy
